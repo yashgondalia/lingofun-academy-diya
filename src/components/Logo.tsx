a980331faaae786bc6db92629167a094dbf7f049
unknown <yashgondalia1981@gmail.com>
2025-05-29 19:23:02 +0530

    all changes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -70,7 +70,7 @@ const Logo: React.FC<LogoProps> = ({
         <img
           src="\lovable-uploads\putlogo.jpg"
           alt="LinGoFun Logo"
-          className="relative z-10 w-[125px] h-[125px] object-cover "
+          className="relative z-10 w-[11git 0px] h-[110px] object-cover "
           style={{ transform: "scale(1.35)" }}
         />
       </div>
